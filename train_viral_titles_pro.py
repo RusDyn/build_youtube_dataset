42b953a89c3af02878c14a25ef7637dfb0621ef2
Dyn <dyn.see@gmail.com>
2025-04-19 00:22:08 +0100

    INTERVAL '3 years'

--- train_viral_titles_pro.py
+++ train_viral_titles_pro.py
@@ -76,7 +76,7 @@ def stage_prep():
         FROM youtube_videos
         WHERE viral_score >= 0.10  -- Adjusted threshold to get ~1k-5k examples
           AND title IS NOT NULL AND description IS NOT NULL
-          AND publishedAt >= (CURRENT_DATE - INTERVAL '2 years')  -- Filter for content less than 2 years old
+          AND publishedAt >= (CURRENT_DATE - INTERVAL '3 years')  -- Filter for content less than 2 years old
         ORDER BY random()
         LIMIT 10000;  -- Added limit to ensure dataset isn't too large
         """
